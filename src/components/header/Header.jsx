f3c3a5f928589631be883aaf665070e48b10506a
MarcosPassos-git <marcosbrunopassos@gmail.com>
2022-08-03 22:57:54 -0300

    Criado componente de icone

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,16 @@
 import styles from './Header.module.css'
 import Title from "../title/Title"
 import Subtitle from "../subtitle/Subtitle"
+import Icon from '../icon/icon'
 
 function Header () {
     return (
         <div className={styles.header}>
             <Title>Jogo da velha</Title>
             <Subtitle>Criado por Marcos Passos</Subtitle>
+            <div className={styles.iconContent}>
+                <Icon iconName="github" link="https://github.com/MarcosPassos-git"/>
+            </div>
         </div>
     )
 }
