385eb38e3ad365090a2b029dc9d02c99839c1b4b
MarcosPassos-git <marcosbrunopassos@gmail.com>
2022-08-11 23:25:03 -0300

    Verificado empate

--- src/components/gameInfo/GameInfo.jsx
+++ src/components/gameInfo/GameInfo.jsx
@@ -2,15 +2,13 @@ import styles from './GameInfo.module.css'
 import Icon from '../icon/Icon'
 import Button from '../button/Button'
 
-function GameInfo ({currentPlayer, winner, onReset}) {
-    const shouldEnableButton = () => {
-        if (winner !== 0) return true
-    }
-
+function GameInfo ({currentPlayer, winner, onReset, isDraw}) {
+    const shouldEnableButton = () => winner !== 0 || isDraw
+    
     return (
         <div className={styles.gameInfo}>
             {
-                winner === 0 &&
+                !isDraw && winner === 0 &&
                 <>                    
                     <h4>Próximo a jogar:</h4>
                     {
@@ -22,7 +20,7 @@ function GameInfo ({currentPlayer, winner, onReset}) {
                 </>
             }
             {
-                winner !== 0 &&
+                !isDraw && winner !== 0 &&
                     <>
                        <h4>Fim de jogo!  Campeão: </h4> 
                        {
@@ -32,6 +30,10 @@ function GameInfo ({currentPlayer, winner, onReset}) {
                         winner === -1 && <Icon iconName="x"/>
                        }
                     </>
+            }
+            {
+                isDraw && <h4>Empate!</h4>
+               
             }
             <Button 
                 onClick={onReset}
