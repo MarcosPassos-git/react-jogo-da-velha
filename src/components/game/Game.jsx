427b9d4acc5d3db0d296b7713836b5b066869bbb
MarcosPassos-git <marcosbrunopassos@gmail.com>
2022-09-25 11:53:54 -0300

    Desafio: Placar

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,6 +1,6 @@
+import { useState, useEffect } from 'react'
 import styles from './Game.module.css'
 import GameOption from '../gameOption/GameOption'
-import { useState, useEffect } from 'react'
 import GameInfo from '../gameInfo/GameInfo'
 import Score from '../score/Score'
 
@@ -21,6 +21,8 @@ function Game () {
     const [winner, setWinner] = useState(0)
     const [winnerLine, setWinnerLine] = useState([])
     const [draw, setDraw] = useState(false)
+    const [xWinnerTimes, setXWinnerTimes] = useState(0)
+    const [circleWinnerTimes, setCircleWinnerTimes] = useState(0)
 
     const handleClick = (pos) => {
         if (gameState[pos] === 0 && winner === 0) {
@@ -37,7 +39,12 @@ function Game () {
             const sum = values.reduce((sum, value) => sum + value)
             if (sum === 3 || sum === -3) {
                 setWinner(sum / 3) 
-                setWinnerLine(line)               
+                setWinnerLine(line) 
+            if (sum > 0) {
+                setCircleWinnerTimes(circleWinnerTimes + 1)
+            } else {
+                setXWinnerTimes(xWinnerTimes + 1)
+            }
             }
         })
     }
@@ -92,7 +99,10 @@ function Game () {
                     isDraw={draw}
                 />                 
             </div>
-            <Score />
+            <Score 
+                circleWinnerTimes={circleWinnerTimes}
+                xWinnerTimes={xWinnerTimes}
+            />
         </>
         
     )
